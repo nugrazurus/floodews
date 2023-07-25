d4bebbc59f1c845cc6bd3f648c88ed3b3851d3c1
nugrazurus <nugrazurus@student.untan.ac.id>
2023-07-25 15:15:14 +0700

    fix basepath on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,49 @@
-import { SensorProps, Sensors, Sidebar } from "@/components";
+"use client";
+import { SensorProps, Sensors } from "@/components";
+import useSWR from "swr";
 
-async function getSensorValue() {
-  const res = await fetch("http://localhost:3000/api/v1/weather");
+async function getSensorValue(url: string) {
+  const res = await fetch(url);
 
   if (!res.ok) throw new Error("Failed to fetch");
 
   return res.json();
 }
 
-export default async function Home() {
-  const sensorValue = await getSensorValue();
+export default function Home() {
+  const basePath = typeof window != "undefined" && window.location.origin;
+
+  const { data, error } = useSWR(
+    `${basePath ? basePath : ""}/api/v1/weather`,
+    getSensorValue
+  );
+
+  if (!data) return <p>Loading...</p>;
+  if (error) return <p>Failed to fetch</p>
+
   const time = new Date();
   const sensors: SensorProps[] = [
     {
       title: "Temperature",
-      value: sensorValue.data ? sensorValue.data.temperature.toFixed(2) : 0.0,
+      value: data.data ? data.data.temperature.toFixed(2) : 0.0,
       unit: "&deg;Celcius",
       icon: "mdi:thermometer",
     },
     {
       title: "Humidity",
-      value: sensorValue.data ? sensorValue.data.humidity.toFixed(2) : 0.0,
+      value: data.data ? data.data.humidity.toFixed(2) : 0.0,
       unit: "%RH",
       icon: "mdi:water-percent-alert",
     },
     {
       title: "Pressure",
-      value: sensorValue.data ? sensorValue.data.pressure.toFixed(2) : 0.0,
+      value: data.data ? data.data.pressure.toFixed(2) : 0.0,
       unit: "hPa",
       icon: "mdi:gauge",
     },
     {
       title: "Altitude",
-      value: sensorValue.data ? sensorValue.data.altitude.toFixed(2) : 0.0,
+      value: data.data ? data.data.altitude.toFixed(2) : 0.0,
       unit: "MSL",
       icon: "mdi:image-filter-hdr",
     },
@@ -43,7 +54,7 @@ export default async function Home() {
         <h1 className="text-lg font-semibold text-gray-700">DASHBOARD</h1>
         <p className="text-md text-gray-500">{time.toDateString()}</p>
       </div>
-      <Sensors sensors={sensors} timestamp={sensorValue.data.timestamp} />
+      <Sensors sensors={sensors} timestamp={data.data.timestamp} />
     </main>
   );
 }
